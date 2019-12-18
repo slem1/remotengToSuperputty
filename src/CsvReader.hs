ae5287564f851c93bb47665611feba15baead279
Sylvain Lemoine <sylvain.lem1@gmail.com>
2019-12-18 20:55:14 +0100

    Simple parser

--- src/CsvReader.hs
+++ src/CsvReader.hs
@@ -1,16 +1,30 @@
 module CsvReader
-    ( cell,
-      line
+    ( parseConnections
     ) where
 
 import Text.Parsec
 import Text.Parsec.String (Parser)
 
+type CsvLine = [String]
+
+data Connection = Connection { name :: String, host :: String, login:: String} deriving (Show)
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-cell :: Parser String
-cell = many $ noneOf ";\n"
+parseCell :: Parser String
+parseCell = many $ noneOf ";\n"
 
-line :: Parser [String]
-line = cell `sepBy` (char ';') 
+parseLine :: Parser CsvLine
+parseLine = parseCell `sepBy` (char ';')
+
+parseLines :: Parser [CsvLine]
+parseLines = parseLine `sepEndBy` (char '\n')
+
+mapToConnection :: CsvLine -> Connection
+mapToConnection [name, host, login] = Connection name host login
+
+parseConnections :: Either ParseError [Connection]
+parseConnections = do 
+    parseResult <- parse parseLines "" "zeg33ssh.plop.com (ihm);zeg33ssh.plop.com;Amuro\nzeg32ssh.plop.com (ihm);zeg32ssh.plop.com;Amuro" 
+    return $ mapToConnection <$> parseResult
