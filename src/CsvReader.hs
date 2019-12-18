11a64940cea4d95d830dacc7af4a9da3ab1a7d6a
Sylvain Lemoine <sylvain.lem1@gmail.com>
2019-12-18 21:31:16 +0100

    parser

--- src/CsvReader.hs
+++ src/CsvReader.hs
@@ -4,15 +4,13 @@ module CsvReader
 
 import Text.Parsec
 import Text.Parsec.String (Parser)
+import Control.Monad
 
 type CsvLine = [String]
 
 data Connection = Connection { name :: String, host :: String, login:: String} deriving (Show)
 
 sampleContent = "name;host;login\nzeg33ssh.plop.com (ihm);zeg33ssh.plop.com;Amuro\nzeg32ssh.plop.com (ihm);zeg32ssh.plop.com;Amuro" 
-
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
 
 parseCell :: Parser String
 parseCell = many $ noneOf ";\n"
@@ -23,10 +21,15 @@ parseLine = parseCell `sepBy` (char ';')
 parseLines :: Parser [CsvLine]
 parseLines = parseLine `sepEndBy` (char '\n')
 
-mapToConnection :: CsvLine -> Connection
-mapToConnection [name, host, login] = Connection name host login
+csvParser :: Parser [CsvLine]
+csvParser =  (manyTill anyChar newline >> parseLines)
 
-parseConnections :: Either ParseError [Connection]
-parseConnections = do 
-    parseResult <- parse (manyTill anyChar newline >> parseLines) "" sampleContent
-    return $ mapToConnection <$> parseResult
+mapToConnections :: [CsvLine] -> [Connection]
+mapToConnections xs = mapToConnection <$> xs
+      where mapToConnection [name, host, login] = Connection name host login
+
+parseConnections :: FilePath -> IO (Either ParseError [Connection])
+parseConnections inputFile = do 
+    content <- readFile inputFile
+    let parsedLines = parse csvParser "" content
+    return $ liftM mapToConnections parsedLines 
