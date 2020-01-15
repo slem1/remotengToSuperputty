ae46699258c39392e6132ef826602f2eb3788488
slem1 <sylvain.lem1@gmail.com>
2020-01-15 22:23:21 +0100

    do everything

--- app/Main.hs
+++ app/Main.hs
@@ -8,18 +8,17 @@ import qualified Data.ByteString.Lazy as BSL
 import Control.Monad.Trans.Except
 import Text.Parsec
 
-main :: IO ()
+--main :: IO ()
 main = do
     args <- getArgs
     case args of 
         [inputFile, outputFile] -> do 
-            result <- runExceptT $ do
-                cnxs <- parseConnections inputFile            
-                let out = renderConnections cnxs
-                return $ BSL.writeFile outputFile out
+            result <- runExceptT $ parseConnections inputFile                            
             case result of 
                 Left error -> putStrLn $ show error   
-                x -> putStrLn "Done!"
+                Right cnxs -> 
+                    let out = renderConnections cnxs in
+                    BSL.writeFile outputFile out
         _ -> error "Usage: app \"inputFile\" \"outputFile\""
 
 --main =     
